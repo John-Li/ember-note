ce7ae3c54fd5b4f0abc9abd6bd9ef52a1d3cf934
ilishchinovskiy <i.lishchinovskiy@svitla.com>
2015-08-30 17:11:25 +0300

    Finished chapter 1

---
+++ tests/acceptance/register-test.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from 'ember-note/tests/helpers/start-app';
+
+module('Acceptance | register', {
+  beforeEach: function() {
+    this.application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(this.application, 'destroy');
+  }
+});
+
+test('visiting /register', function(assert) {
+  server.logging = true;
+
+  visit('/register');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/register');
+  });
+});
